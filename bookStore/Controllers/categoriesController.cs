31b68be725da650ec73728db447df043623b1f0f
ahmad anini <ahmadanini86@gmail.com>
2024-05-18 18:56:20 +0300

    show categories

---
+++ bookStore/Controllers/categoriesController.cs
@@ -0,0 +1,19 @@
+﻿using bookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookStore.Controllers
+{
+    public class categoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public categoriesController(ApplicationDbContext context) {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = context.categories.ToList();
+            return View(categories);
+        }
+    }
+}
--- bookStore/Data/ApplicationDbContext.cs
+++ bookStore/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using bookStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace bookStore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Category> categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
